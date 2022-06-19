d5693ea486629c4d021f66ef699aada9a673b669
Mihir S <mihir.solankar1@gmail.com>
2022-06-19 22:55:52 +0530

    App run on server port

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets.router');
@@ -7,6 +8,12 @@ app.use(cors({
     origin:'http://localhost:3000'
 }));
 app.use(express.json());
-app.use('/',planetsRouter);
+app.use(express.static(path.join(__dirname,'..','public')));
+app.use(planetsRouter);
+
+//enabling the build to run on same port as server
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public','index.html'));
+});
 
 module.exports = app;
